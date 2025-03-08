7054ba7da011497c8233991c5769e7b2fca74abf
jang-199 <jw1010110@gmail.com>
2025-03-09 08:11:07 +0900

    Data module UserService, 유저 저장, 검색, 이름 업데이트, 닉네임 업데이트, 번호 업데이트 구현

--- onething/data/core-data/src/main/java/com/clip/user/service/UserService.java
+++ onething/data/core-data/src/main/java/com/clip/user/service/UserService.java
@@ -1,12 +1,46 @@
 package com.clip.user.service;
 
+import com.clip.user.entity.Platform;
+import com.clip.user.entity.User;
+import com.clip.user.exception.NicknameAlreadyExistsException;
+import com.clip.user.exception.PhoneNumberAlreadyExistsException;
 import com.clip.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
+    public Optional<User> findOptUser(String socialId, Platform platform) {
+        return userRepository.findUser(socialId, platform);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void updatePhoneNumber(long userId, String phoneNumber) {
+        try {
+            userRepository.updatePhoneNumber(userId, phoneNumber);
+        }catch (DataIntegrityViolationException e){
+            throw new PhoneNumberAlreadyExistsException("이미 사용중인 번호로 업데이트 할 수 없습니다.");
+        }
+    }
+
+    public void updateUserName(long userId, String userName) {
+        userRepository.updateUserName(userId, userName);
+    }
+
+    public void updateNickname(long userId, String nickname) {
+        try {
+            userRepository.updateNickname(userId, nickname);
+        }catch (DataIntegrityViolationException e){
+            throw new NicknameAlreadyExistsException("이미 사용중인 닉네임으로 업데이트 할 수 없습니다.");
+        }
+    }
 }
