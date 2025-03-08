e91fa7f848bfe653c22263248fd393352ff4d4d6
jang-199 <jw1010110@gmail.com>
2025-03-09 08:23:58 +0900

    API module 회원 계정 생성 및 정보 수정 서비스 구현

---
+++ onething/boot/api/src/main/java/com/clip/api/user/service/UserAccountService.java
@@ -0,0 +1,64 @@
+package com.clip.api.user.service;
+
+import com.clip.api.user.controller.dto.SignupDto;
+import com.clip.auth.entity.Token;
+import com.clip.auth.service.TokenService;
+import com.clip.global.config.jwt.TokenProvider;
+import com.clip.user.entity.TermsAcceptance;
+import com.clip.user.entity.User;
+import com.clip.user.service.TermsAcceptanceService;
+import com.clip.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountService {
+    private final TokenProvider tokenProvider;
+    private final UserService userService;
+    private final TermsAcceptanceService termsAcceptanceService;
+    private final TokenService tokenService;
+
+    @Transactional
+    public TokenProvider.Token signup(SignupDto.Request request) {
+        Optional<User> optUser = userService.findOptUser(request.getSocialId(), request.getPlatform());
+
+        if (optUser.isPresent()) {
+            TokenProvider.Token token = tokenProvider.generateToken(optUser.get().getId(), LocalDateTime.now());
+            tokenService.updateRefreshToken(optUser.get(), token.refreshToken());
+            return token;
+        }
+
+        User user = userService.save(User.builder()
+                .socialId(request.getSocialId())
+                .platform(request.getPlatform())
+                .build());
+        termsAcceptanceService.save(TermsAcceptance.builder()
+                .servicePermission(request.isServicePermission())
+                .privatePermission(request.isPrivatePermission())
+                .marketingPermission(request.isMarketingPermission())
+                .user(user)
+                .build()
+        );
+
+        TokenProvider.Token token = tokenProvider.generateToken(user.getId(), LocalDateTime.now());
+        tokenService.save(Token.builder().refreshToken(token.refreshToken()).user(user).build());
+        return token;
+    }
+
+    public void updatePhoneNumber(long userId, String phoneNumber) {
+        userService.updatePhoneNumber(userId, phoneNumber);
+    }
+
+    public void updateName(long userId, String userName) {
+        userService.updateUserName(userId, userName);
+    }
+
+    public void updateNickname(long userId, String nickname) {
+        userService.updateNickname(userId, nickname);
+    }
+}
