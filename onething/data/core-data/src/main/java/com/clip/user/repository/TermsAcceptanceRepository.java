19f48fe7301a9f247caf96171f6b840a1ce642a4
jang-199 <jw1010110@gmail.com>
2025-03-09 08:20:33 +0900

    Data module, 약관 Repository 구현

---
+++ onething/data/core-data/src/main/java/com/clip/user/repository/TermsAcceptanceRepository.java
@@ -0,0 +1,7 @@
+package com.clip.user.repository;
+
+import com.clip.user.entity.TermsAcceptance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermsAcceptanceRepository extends JpaRepository<TermsAcceptance, Long> {
+}
