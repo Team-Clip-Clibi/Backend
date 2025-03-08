47fd095d1c917bb11c3f5a5b362d18c6388b0b26
jang-199 <jw1010110@gmail.com>
2025-03-09 08:24:39 +0900

    번호, 닉네임 중복 사용 불가 예외 핸들러 구현

---
+++ onething/boot/api/src/main/java/com/clip/global/aop/ValidExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.clip.global.aop;
+
+import com.clip.user.exception.NicknameAlreadyExistsException;
+import com.clip.user.exception.PhoneNumberAlreadyExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ValidExceptionHandler {
+    @ExceptionHandler(PhoneNumberAlreadyExistsException.class)
+    public ResponseEntity<?> phoneNumberAlreadyExistsException(PhoneNumberAlreadyExistsException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(NicknameAlreadyExistsException.class)
+    public ResponseEntity<?> nicknameAlreadyExistsException(NicknameAlreadyExistsException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+
+
+}
