d043fc14e93382ee82e9df88719cd84156015190
jang-199 <jw1010110@gmail.com>
2025-03-10 01:24:30 +0900

    url 관련 어노테이션과 Request Body 어노테이션 인터페이스로 이동

--- onething/boot/api/src/main/java/com/clip/api/docs/user/UserAccountDocs.java
+++ onething/boot/api/src/main/java/com/clip/api/docs/user/UserAccountDocs.java
@@ -3,12 +3,18 @@ package com.clip.api.docs.user;
 import com.clip.api.user.controller.dto.*;
 import com.clip.global.config.jwt.TokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "회원관리", description = "회원가입, 로그인, 전화번호 업데이트 etc")
+@RequestMapping("/users")
 public interface UserAccountDocs {
 
 
@@ -19,6 +25,15 @@ public interface UserAccountDocs {
                     Access Token과 Refresh Token을 발급하여 반환합니다.
                     """
     )
+    @ApiResponse(
+            responseCode = "200",
+            description = "회원 가입 성공",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = TokenProvider.Token.class)
+            )
+    )
+    @PostMapping("/signup")
     TokenProvider.Token createUserAccount(@RequestBody SignupDto request);
 
     @Operation(
@@ -29,6 +44,24 @@ public interface UserAccountDocs {
                     단, 가입되지 않은 유저일 경우 HTTP 400 Bad Request를 반환합니다.
                     """
     )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "로그인 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TokenProvider.Token.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "로그인 실패",
+                    content = @Content(
+                            schema = @Schema()
+                    )
+            )
+    })
+    @PostMapping("/signin")
     TokenProvider.Token loginUserAccount(@RequestBody LoginDto request);
 
     @Operation(
@@ -39,6 +72,18 @@ public interface UserAccountDocs {
                     다른 유저가 사용중인 번호일 경우 HTTP 400 Bad Request를 반환합니다.
                     """
     )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "번호 업데이트 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "번호 업데이트 실패"
+            )
+    })
+    @PatchMapping("/phone")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void updatePhoneNumber(@RequestBody UpdatePhoneNumberDto updatePhoneNumberDto,
                                            @AuthenticationPrincipal UserDetails userDetails);
 
@@ -48,6 +93,12 @@ public interface UserAccountDocs {
                     유저의 이름을 업데이트합니다.
                     """
     )
+    @ApiResponse(
+            responseCode = "200",
+            description = "유저 이름 업데이트 성공"
+    )
+    @PatchMapping("/name")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void updateName(@RequestBody UpdateNameDto updateNameDto,
                                     @AuthenticationPrincipal UserDetails userDetails);
 
@@ -58,6 +109,18 @@ public interface UserAccountDocs {
                     다른 유저가 사용중인 닉네임일 경우 HTTP 400 Bad Request를 반환합니다.
                     """
     )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "닉네임 업데이트 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "닉네임 업데이트 실패"
+            )
+    })
+    @PatchMapping("/nickname")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     void updateNickname(@RequestBody UpdateNicknameDto updateNicknameDto,
                                         @AuthenticationPrincipal UserDetails userDetails);
 }
--- onething/boot/api/src/main/java/com/clip/api/user/controller/UserAccountController.java
+++ onething/boot/api/src/main/java/com/clip/api/user/controller/UserAccountController.java
@@ -10,40 +10,36 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/users")
+
 @RestController
 @RequiredArgsConstructor
 public class UserAccountController implements UserAccountDocs {
     private final UserAccountService userAccountService;
 
-    @PostMapping("/signup")
-    public TokenProvider.Token createUserAccount(@RequestBody SignupDto request) {
+    @Override
+    public TokenProvider.Token createUserAccount(SignupDto request) {
         return userAccountService.signup(request);
     }
 
-    @PostMapping("/signin")
-    public TokenProvider.Token loginUserAccount(@RequestBody LoginDto request) {
+    @Override
+    public TokenProvider.Token loginUserAccount(LoginDto request) {
         return userAccountService.login(request);
     }
 
-    @PatchMapping("/phone")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updatePhoneNumber(@RequestBody UpdatePhoneNumberDto updatePhoneNumberDto,
-                                                  @AuthenticationPrincipal UserDetails userDetails) {
+    @Override
+    public void updatePhoneNumber(UpdatePhoneNumberDto updatePhoneNumberDto,
+                                                  UserDetails userDetails) {
         userAccountService.updatePhoneNumber(Long.parseLong(userDetails.getUsername()), updatePhoneNumberDto.getPhoneNumber());
     }
 
-    @PatchMapping("/name")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateName(@RequestBody UpdateNameDto updateNameDto,
-                                           @AuthenticationPrincipal UserDetails userDetails) {
+    @Override
+    public void updateName(UpdateNameDto updateNameDto,
+                                           UserDetails userDetails) {
         userAccountService.updateName(Long.parseLong(userDetails.getUsername()), updateNameDto.getUserName());
     }
 
-    @PatchMapping("/nickname")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateNickname(@RequestBody UpdateNicknameDto updateNicknameDto,
-                                               @AuthenticationPrincipal UserDetails userDetails) {
+    public void updateNickname(UpdateNicknameDto updateNicknameDto,
+                                               UserDetails userDetails) {
         userAccountService.updateNickname(Long.parseLong(userDetails.getUsername()), updateNicknameDto.getNickname());
     }
 }
